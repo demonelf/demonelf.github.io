3d621ed6ef541b83cbd79cf923867ac211248917
madhex <demonelf@madhex.com>
2019-08-28 15:27:20 +0800

    1.自动更新

--- source/_posts/EMBEDDED/linux 下jansson安装和使用.md
+++
@@ -1,7 +0,0 @@
----
-title: linux 下jansson安装和使用
-date: 2017-07-12 15:19:06
-tags:
----
-
-http://www.cnblogs.com/etangyushan/p/3724017.html
--- source/_posts/new.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-export PATH="/usr/local/bin:/usr/bin:/bin:/opt/bin:/c/Windows/System32:/c/Windows:/c/Windows/System32/Wbem:/c/Windows/System32/WindowsPowerShell/v1.0/:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/opt/toolchain/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin:/opt/FriendlyARM/toolschain/4.4.3/bin:/c/Users/jimmy/AppData/Roaming/npm:/c/Program Files/nodejs"
-cd /e/hexo/
-hexo new "new"
